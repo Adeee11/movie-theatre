54fde5a172c14a990f929c4bc91337bea87d0e80
AnmolPreet <anmolpreet@techhiedunia.com>
2020-09-29 22:29:42 +0530

    Added Netlify Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,26 @@ module.exports = {
         // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/admin/*": [
+            "Basic-Auth: admin:admin",
+          ],
+          "/admin": [
+            // matching headers (by type) are replaced by Netlify with more specific routes
+            "Basic-Auth: admin:admin",
+          ],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
